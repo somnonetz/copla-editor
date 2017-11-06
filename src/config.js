9444be6082f9490ea7efaf0f197019a6438558a9
Max Beier <beierm@htw-berlin.de>
2017-11-06 21:52:48 +0100

    added config

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const autologin = false;
+
+const host = 'https://xnat.tld/xnat/REST';
+
+const credentials = {
+  username: 'admin',
+  password: 'password',
+};
+
+const defaultProject = 'project';
+const defaultSubject = 'subject';
+
+export { autologin, host, credentials, defaultProject, defaultSubject };
