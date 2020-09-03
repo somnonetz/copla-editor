ef9bd437976a87601c6f7654912f48309a8828f0
jmz-b <jaymuhz.bee@gmail.com>
2020-09-03 14:40:10 +0200

    remove .env from vcs

--- sn-editor/src/config.js
+++ sn-editor/src/config.js
@@ -1,13 +1,28 @@
-const autologin = false;
 
-const host = process.env.REACT_APP_XNAT_API_URL;
 // const host = 'http://localhost/xnat/REST';
+const host = process.env.REACT_APP_XNAT_API_URL;
 
+// enable auto login
+// example
+// const credentials = {
+//     username: 'admin',
+//     password: 'admin',
+// };
+const autologin = false;
 const credentials = {};
 
+// enable reconstruction pipeline
+// example:
+// const doReconstruction = true;
+// const pipelineName = 'somnonetz-pipeline';
+// const pipelineParams = {
+//   algorithm: 'sn_getEDFHeaderdata'
+// };
 const doReconstruction = false;
-const doAsclepiosUpload = true;
 const pipelineName = null;
 const pipelineParams = {};
+
+// enable ASCLEPIOS integration
+const doAsclepiosUpload = process.env.REACT_APP_DO_ASCLEPIOS_UPLOAD;
 
 export { autologin, host, credentials, pipelineName, pipelineParams, doReconstruction, doAsclepiosUpload };
